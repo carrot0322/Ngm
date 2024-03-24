89717da084ef5cd5bccfcf4862eff99ff35d25f1
carrot0322 <carrot100322@gmail.com>
2024-03-24 22:28:51 +0900

    nofall grim2b2t rename

--- src/main/java/me/coolmint/ngm/mixin/MixinClientPlayerEntity.java
+++ src/main/java/me/coolmint/ngm/mixin/MixinClientPlayerEntity.java
@@ -1,18 +1,29 @@
 package me.coolmint.ngm.mixin;
 
+import com.mojang.authlib.GameProfile;
 import me.coolmint.ngm.event.Stage;
+import me.coolmint.ngm.event.impl.EventSync;
 import me.coolmint.ngm.event.impl.UpdateEvent;
 import me.coolmint.ngm.event.impl.UpdateWalkingPlayerEvent;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static me.coolmint.ngm.features.modules.Module.fullNullCheck;
+import static me.coolmint.ngm.util.player.PlayerUtils.isSprinting;
 import static me.coolmint.ngm.util.traits.Util.EVENT_BUS;
 
 @Mixin(ClientPlayerEntity.class)
-public class MixinClientPlayerEntity {
+public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
+    public MixinClientPlayerEntity(ClientWorld world, GameProfile profile) {
+        super(world, profile);
+    }
+
     @Inject(method = "tick", at = @At("TAIL"))
     private void tickHook(CallbackInfo ci) {
         EVENT_BUS.post(new UpdateEvent());
@@ -26,5 +37,14 @@ public class MixinClientPlayerEntity {
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;sendMovementPackets()V", shift = At.Shift.AFTER))
     private void tickHook3(CallbackInfo ci) {
         EVENT_BUS.post(new UpdateWalkingPlayerEvent(Stage.POST));
+    }
+
+    @Inject(method = "sendMovementPackets", at = @At("HEAD"), cancellable = true)
+    private void sendMovementPacketsHook(CallbackInfo info) {
+        if (fullNullCheck()) return;
+        EventSync event = new EventSync(getYaw(), getPitch());
+        EVENT_BUS.post(event);
+
+        if (event.isCancelled()) info.cancel();
     }
 }
