2981ac74b8293965d633337b7a9c28d8bb471c01
carrot0322 <carrot100322@gmail.com>
2024-03-25 02:02:31 +0900

    GameLeftEvent 2

--- src/main/java/me/coolmint/ngm/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/me/coolmint/ngm/mixin/MixinClientPlayNetworkHandler.java
@@ -1,12 +1,16 @@
 package me.coolmint.ngm.mixin;
 
 import me.coolmint.ngm.event.impl.ChatEvent;
+import me.coolmint.ngm.event.impl.GameLeftEvent;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.EnterReconfigurationS2CPacket;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static me.coolmint.ngm.features.modules.Module.fullNullCheck;
 import static me.coolmint.ngm.util.traits.Util.EVENT_BUS;
 
 @Mixin(ClientPlayNetworkHandler.class)
@@ -16,5 +20,18 @@ public class MixinClientPlayNetworkHandler {
         ChatEvent event = new ChatEvent(content);
         EVENT_BUS.post(event);
         if (event.isCancelled()) ci.cancel();
+    }
+
+    @Inject(method = "onGameJoin", at = @At("TAIL"))
+    private void onGameJoinTail(GameJoinS2CPacket packet, CallbackInfo info) {
+        if (fullNullCheck()) {
+            EVENT_BUS.post(GameLeftEvent.get());
+        }
+    }
+
+    // the server sends a GameJoin packet after the reconfiguration phase
+    @Inject(method = "onEnterReconfiguration", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V", shift = At.Shift.AFTER))
+    private void onEnterReconfiguration(EnterReconfigurationS2CPacket packet, CallbackInfo info) {
+        EVENT_BUS.post(GameLeftEvent.get());
     }
 }
