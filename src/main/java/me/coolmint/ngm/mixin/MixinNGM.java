54598fe04dccd876559bab186396481d75fe4c0e
carrot0322 <carrot100322@gmail.com>
2024-04-01 21:56:21 +0900

    믹스인수정

--- src/main/java/me/coolmint/ngm/mixin/MixinNGM.java
+++ src/main/java/me/coolmint/ngm/mixin/MixinNGM.java
@@ -4,7 +4,10 @@ import me.coolmint.ngm.auth.Init;
 import me.coolmint.ngm.features.gui.fonts.FontRenderers;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.RunArgs;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,22 +17,14 @@ import java.io.IOException;
 
 @Mixin(MinecraftClient.class)
 public class MixinNGM {
+    @Shadow @Final private static Logger LOGGER;
+
     @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setOverlay(Lnet/minecraft/client/gui/screen/Overlay;)V", shift = At.Shift.BEFORE))
     public void init(RunArgs args, CallbackInfo ci) {
         if (!Init.auth()) {
-            try {
-                Init.sendFailed();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            //Send Log
+            LOGGER.warn("Invalid Hwid : " + Init.encodeHWID(Init.getHwid()));
             System.exit(1);
-        }
-        else {
-            try {
-                Init.sendWebhook();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
         try {
