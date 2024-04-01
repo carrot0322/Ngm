3dad3e25e31d6a5458c43f0b54e6334203695b41
2bkrplayer <2bkrplayer@gmail.com>
2024-04-01 22:05:31 +0900

    trigger swordonly mode

--- src/main/java/me/coolmint/ngm/features/modules/legit/Trigger.java
+++ src/main/java/me/coolmint/ngm/features/modules/legit/Trigger.java
@@ -4,19 +4,27 @@ import me.coolmint.ngm.features.modules.Module;
 import me.coolmint.ngm.features.settings.Setting;
 import me.coolmint.ngm.util.MathUtil;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 
 public class Trigger extends Module {
     public Setting<Integer> chance = register(new Setting<>("Chance", 100, 0, 100));
+    public Setting<Boolean> SwordOnly = register(new Setting<>("Sword Only", false));
 
     public Trigger() {
         super("Trigger", "", Category.LEGIT, true, false, false);
     }
 
     @Override
-    public void onUpdate(){
-        if(fullNullCheck() && !mc.isWindowFocused()) return;
+    public void onUpdate() {
+        if (fullNullCheck() && !mc.isWindowFocused()) {
+            return;
+        }
+        if (SwordOnly.getValue() && !SwordCheck()) {
+            return;
+        }
 
         if (mc.crosshairTarget instanceof EntityHitResult entityHitResult) {
             if (MathUtil.random(0, 100) <= chance.getValue() && mc.player.getAttackCooldownProgress(0.5f) == 1) {
@@ -26,10 +34,12 @@ public class Trigger extends Module {
         }
     }
 
-    private void attack(Entity entity){
+    private void attack(Entity entity) {
         mc.interactionManager.attackEntity(mc.player, entity);
         mc.player.swingHand(Hand.MAIN_HAND);
     }
-
-
+    public boolean SwordCheck() {
+        ItemStack getItem = mc.player.getMainHandStack();
+        return (getItem.isOf(Items.NETHERITE_SWORD) || getItem.isOf(Items.DIAMOND_SWORD) || getItem.isOf(Items.GOLDEN_SWORD) || getItem.isOf(Items.IRON_SWORD) || getItem.isOf(Items.STONE_SWORD) || getItem.isOf(Items.WOODEN_SWORD));
+    }
 }
