381e454dba77846366d03fc2d80a656e5d690ab1
carrot0322 <carrot100322@gmail.com>
2024-04-07 01:46:07 +0900

    Scaffold 에러 200개 남았다

--- src/main/java/me/coolmint/ngm/mixin/ILivingEntity.java
+++ src/main/java/me/coolmint/ngm/mixin/ILivingEntity.java
@@ -1,8 +1,14 @@
 package me.coolmint.ngm.mixin;
 
+import me.coolmint.ngm.event.impl.JumpEvent;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
+
+import static me.coolmint.ngm.util.traits.Util.EVENT_BUS;
 
 @Mixin(LivingEntity.class)
 public interface ILivingEntity {
@@ -14,4 +20,5 @@ public interface ILivingEntity {
 
     @Accessor("jumpingCooldown")
     void setLastJumpCooldown(int val);
+
 }
