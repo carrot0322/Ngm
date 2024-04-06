2d1c5e02881d81f94f54254cfc1d2ec15603abf3
carrot0322 <carrot100322@gmail.com>
2024-04-06 17:08:16 +0900

    bow aim 좆같네요 하@@

---
+++ src/main/java/me/coolmint/ngm/util/misc/Producer.java
@@ -0,0 +1,5 @@
+package me.coolmint.ngm.util.misc;
+
+public interface Producer<T> {
+    T create();
+}
--- src/main/java/me/coolmint/ngm/util/player/RotationUtils.java
+++ src/main/java/me/coolmint/ngm/util/player/RotationUtils.java
@@ -1,22 +1,22 @@
 package me.coolmint.ngm.util.player;
 
-import me.coolmint.ngm.Ngm;
-import net.minecraft.client.MinecraftClient;
+import me.coolmint.ngm.util.entity.Target;
+import me.coolmint.ngm.util.misc.Pool;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 import static me.coolmint.ngm.util.traits.Util.mc;
 
-public enum RotationUtils
+public class RotationUtils
 {
-    ;
-
     public static Vec3d getEyesPos()
     {
         ClientPlayerEntity player = mc.player;
@@ -140,34 +140,109 @@ public enum RotationUtils
         return ((a - b) % 360.0f + 540.0f) % 360.0f - 180.0f;
     }
 
-    public static void faceBow(Entity target, boolean silent, boolean predict, float predictSize) {
-        ClientPlayerEntity player = mc.player;
+    public static double getYaw(Entity entity) {
+        return mc.player.getYaw() + MathHelper.wrapDegrees((float) Math.toDegrees(Math.atan2(entity.getZ() - mc.player.getZ(), entity.getX() - mc.player.getX())) - 90f - mc.player.getYaw());
+    }
 
-        double posX = target.getX() + (predict ? (target.getX() - target.prevX) * predictSize : 0) - (player.getX() + (predict ? player.getX() - player.prevX : 0));
-        double posY = target.getBoundingBox().minY + (predict ? (target.getBoundingBox().minY - target.prevY) * predictSize : 0) + target.getEyeY() - 0.15 - (player.getBoundingBox().minY + (predict ? player.getY() - player.prevY : 0)) - player.getEyeY();
-        double posZ = target.getZ() + (predict ? (target.getZ() - target.prevZ) * predictSize : 0) - (player.getZ() + (predict ? player.getZ() - player.prevZ : 0));
+    public static double getYaw(Vec3d pos) {
+        return mc.player.getYaw() + MathHelper.wrapDegrees((float) Math.toDegrees(Math.atan2(pos.getZ() - mc.player.getZ(), pos.getX() - mc.player.getX())) - 90f - mc.player.getYaw());
+    }
 
-        double posSqrt = Math.sqrt(posX * posX + posZ * posZ);
+    public static double getPitch(Vec3d pos) {
+        double diffX = pos.getX() - mc.player.getX();
+        double diffY = pos.getY() - (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()));
+        double diffZ = pos.getZ() - mc.player.getZ();
 
-        float velocity = Ngm.moduleManager.isModuleEnabled("BowSpam") ? 1f : player.getItemUseTime() / 20f;
-        velocity = (velocity * velocity + velocity * 2) / 3;
-        if (velocity > 1) velocity = 1f;
+        double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
 
-        float yaw = (float) (Math.atan2(posZ, posX) * 180 / Math.PI) - 90f;
-        float pitch = (float) -Math.toDegrees(Math.atan((velocity * velocity - Math.sqrt(velocity * velocity * velocity * velocity - 0.006f * (0.006f * (posSqrt * posSqrt) + 2 * posY * (velocity * velocity)))) / (0.006f * posSqrt)));
+        return mc.player.getPitch() + MathHelper.wrapDegrees((float) -Math.toDegrees(Math.atan2(diffY, diffXZ)) - mc.player.getPitch());
+    }
 
-        if (silent) {
-            //setTargetRotation(new Rotation(yaw, pitch));
-        } else {
-            Rotation currentRotation = new Rotation(player.getYaw(), player.getPitch());
-            Rotation targetRotation = limitAngleChange(currentRotation, new Rotation(yaw, pitch), (10 + new Random().nextInt(6)));
-            targetRotation.toPlayer(mc.player);
+    public static double getPitch(Entity entity, Target target) {
+        double y;
+        if (target == Target.Head) y = entity.getEyeY();
+        else if (target == Target.Body) y = entity.getY() + entity.getHeight() / 2;
+        else y = entity.getY();
+
+        double diffX = entity.getX() - mc.player.getX();
+        double diffY = y - (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()));
+        double diffZ = entity.getZ() - mc.player.getZ();
+
+        double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
+
+        return mc.player.getPitch() + MathHelper.wrapDegrees((float) -Math.toDegrees(Math.atan2(diffY, diffXZ)) - mc.player.getPitch());
+    }
+
+    public static double getPitch(Entity entity) {
+        return getPitch(entity, Target.Body);
+    }
+
+    public static double getYaw(BlockPos pos) {
+        return mc.player.getYaw() + MathHelper.wrapDegrees((float) Math.toDegrees(Math.atan2(pos.getZ() + 0.5 - mc.player.getZ(), pos.getX() + 0.5 - mc.player.getX())) - 90f - mc.player.getYaw());
+    }
+
+    public static double getPitch(BlockPos pos) {
+        double diffX = pos.getX() + 0.5 - mc.player.getX();
+        double diffY = pos.getY() + 0.5 - (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()));
+        double diffZ = pos.getZ() + 0.5 - mc.player.getZ();
+
+        double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
+
+        return mc.player.getPitch() + MathHelper.wrapDegrees((float) -Math.toDegrees(Math.atan2(diffY, diffXZ)) - mc.player.getPitch());
+    }
+
+    private static final Pool<Rotation2> rotationPool = new Pool<>(Rotation2::new);
+    private static final List<Rotation2> rotations = new ArrayList<>();
+
+    public static void rotate(double yaw, double pitch, int priority, boolean clientSide, Runnable callback) {
+        Rotation2 rotation = rotationPool.get();
+        rotation.set(yaw, pitch, priority, clientSide, callback);
+
+        int i = 0;
+        for (; i < rotations.size(); i++) {
+            if (priority > rotations.get(i).priority) break;
+        }
+
+        rotations.add(i, rotation);
+    }
+
+    public static void rotate(double yaw, double pitch, int priority, Runnable callback) {
+        rotate(yaw, pitch, priority, false, callback);
+    }
+
+    public static void rotate(double yaw, double pitch, Runnable callback) {
+        rotate(yaw, pitch, 0, callback);
+    }
+
+    public static void rotate(double yaw, double pitch, int priority) {
+        rotate(yaw, pitch, priority, null);
+    }
+
+    public static void rotate(double yaw, double pitch) {
+        rotate(yaw, pitch, 0, null);
+    }
+
+    private static class Rotation2 {
+        public double yaw, pitch;
+        public int priority;
+        public boolean clientSide;
+        public Runnable callback;
+
+        public void set(double yaw, double pitch, int priority, boolean clientSide, Runnable callback) {
+            this.yaw = yaw;
+            this.pitch = pitch;
+            this.priority = priority;
+            this.clientSide = clientSide;
+            this.callback = callback;
+        }
+
+        public void sendPacket() {
+            mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.LookAndOnGround((float) yaw, (float) pitch, mc.player.isOnGround()));
+            runCallback();
+        }
+
+        public void runCallback() {
+            if (callback != null) callback.run();
         }
     }
-
-    /*
-    public static void setTargetRotation(Rotation rotation) {
-        setTargetRotation(rotation);
-    }
-     */
 }
