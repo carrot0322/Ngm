00986d8296a9e1b340a30e584f6acfc6e73c307e
carrot0322 <carrot100322@gmail.com>
2024-04-26 23:10:41 +0900

    Update Hud

--- src/main/java/me/coolmint/ngm/features/modules/client/HudModule.java
+++ src/main/java/me/coolmint/ngm/features/modules/client/HudModule.java
@@ -10,12 +10,19 @@ import me.coolmint.ngm.util.client.ColorUtil;
 import net.minecraft.client.MinecraftClient;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class HudModule extends Module {
     private final Setting<Boolean> Watermark = register(new Setting<>("Watermark", true));
     private final Setting<Boolean> Arraylist = register(new Setting<>("Arraylist", true));
+    private final Setting<Boolean> Info = register(new Setting<>("Info", true));
+    private final Setting<Boolean> Coordinate = register(new Setting<>("Coordinate", true, v -> Info.getValue()));
+    private final Setting<Boolean> Fps = register(new Setting<>("Fps", true, v -> Info.getValue()));
+    private final Setting<Boolean> Tps = register(new Setting<>("Tps", true, v -> Info.getValue()));
+    private final Setting<Boolean> Brand = register(new Setting<>("Server Brand", false, v -> Info.getValue()));
+    private final Setting<Boolean> Speed = register(new Setting<>("Speed", true, v -> Info.getValue()));
     public Setting<Boolean> smoothFont = register(new Setting<>("SmoothFont", true));
     public Setting<Boolean> rainbow = register(new Setting<>("Rainbow", false));
     public Setting<Integer> rainbowHue = register(new Setting<>("Delay", 240, 0, 600, v -> rainbow.getValue()));
@@ -80,34 +87,94 @@ public class HudModule extends Module {
                 float stringWidth = FontRenderers.Hud.getStringWidth(displayName);
                 float stringWidthMC = mc.textRenderer.getWidth(displayName);
 
+                float x = smoothFont.getValue() ? mc.getWindow().getScaledWidth() - stringWidth - 2 : mc.getWindow().getScaledWidth() - stringWidthMC -2;
                 float yOffset = mc.textRenderer.fontHeight + 2;
 
                 if(smoothFont.getValue()){
                     if(rainbow.getValue())
                         FontRenderers.Hud.drawString(
                                 event.getContext().getMatrices(), displayName,
-                                mc.getWindow().getScaledWidth() - stringWidth - 2, y, getRainbow(alpha.getValue()), true
+                                x, y, getRainbow(alpha.getValue()), true
                         );
                     else
                         FontRenderers.Hud.drawString(
                                 event.getContext().getMatrices(), displayName,
-                                mc.getWindow().getScaledWidth() - stringWidth - 2, y, new Color(red.getValue(), green.getValue(), blue.getValue(), alpha.getValue()).getRGB(), true
+                                x, y, new Color(red.getValue(), green.getValue(), blue.getValue(), alpha.getValue()).getRGB(), true
                         );
                 } else {
                     if(rainbow.getValue()){
                         event.getContext().drawTextWithShadow(
                                 mc.textRenderer, displayName,
-                                (int) (mc.getWindow().getScaledWidth() - stringWidthMC - 2), y, getRainbow(alpha.getValue())
+                                (int) x, y, getRainbow(alpha.getValue())
                         );
                     } else {
                         event.getContext().drawTextWithShadow(
                                 mc.textRenderer, displayName,
-                                (int) (mc.getWindow().getScaledWidth() - stringWidthMC - 2), y, getRainbow(alpha.getValue())
+                                (int) x, y, getRainbow(alpha.getValue())
                         );
                     }
                 }
 
                 y += yOffset;
+            }
+        }
+
+        if(Info.getValue()){
+            if(mc.player == null) return;
+            List<String> items = new ArrayList<>();
+
+            if(Coordinate.getValue())
+                items.add("X: " + String.format("%.2f", mc.player.getX()) + " Y: " + String.format("%.2f", mc.player.getY()) + " Z: " + String.format("%.2f", mc.player.getZ()));
+
+            if(Fps.getValue())
+                items.add(mc.getCurrentFps() + " FPS");
+
+            if(Tps.getValue())
+                items.add(Ngm.serverManager.getTPS() + " TPS");
+
+            if(Brand.getValue())
+                items.add(Ngm.serverManager.getServerBrand());
+
+            if(Speed.getValue())
+                items.add(Ngm.speedManager.getSpeedKpH() + " KPH");
+
+            items.sort(Comparator.comparingDouble(mod -> smoothFont.getValue() ? -FontRenderers.Hud.getStringWidth(mod) : -mc.textRenderer.getWidth(mod)));
+
+            int y = mc.getWindow().getScaledHeight() - 10;
+
+            for (String mod : items) {
+                float stringWidth = FontRenderers.Hud.getStringWidth(mod);
+                float stringWidthMC = mc.textRenderer.getWidth(mod);
+                float x = smoothFont.getValue() ? mc.getWindow().getScaledWidth() - stringWidth - 2 : mc.getWindow().getScaledWidth() - stringWidthMC -2;
+
+                float yOffset = smoothFont.getValue() ? FontRenderers.Hud.getFontHeight() - 2 : mc.textRenderer.fontHeight - 2;
+
+                if(smoothFont.getValue()){
+                    if(rainbow.getValue())
+                        FontRenderers.Hud.drawString(
+                                event.getContext().getMatrices(), mod,
+                                x, y, getRainbow(alpha.getValue()), true
+                        );
+                    else
+                        FontRenderers.Hud.drawString(
+                                event.getContext().getMatrices(), mod,
+                                x, y, new Color(red.getValue(), green.getValue(), blue.getValue(), alpha.getValue()).getRGB(), true
+                        );
+                } else {
+                    if(rainbow.getValue()){
+                        event.getContext().drawTextWithShadow(
+                                mc.textRenderer, mod,
+                                (int) x, y, getRainbow(alpha.getValue())
+                        );
+                    } else {
+                        event.getContext().drawTextWithShadow(
+                                mc.textRenderer, mod,
+                                (int) x, y, getRainbow(alpha.getValue())
+                        );
+                    }
+                }
+
+                y -= yOffset;
             }
         }
     }
